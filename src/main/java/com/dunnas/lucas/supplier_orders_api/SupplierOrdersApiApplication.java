bbe688e46b44baaf8e1292e5e7df9575992958b1
Lucas <lugab037>
2025-08-06 17:56:38 -0300

    first commit

---
+++ src/main/java/com/dunnas/lucas/supplier_orders_api/SupplierOrdersApiApplication.java
@@ -0,0 +1,13 @@
+package com.dunnas.lucas.supplier_orders_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierOrdersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierOrdersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dunnas/lucas/supplier_orders_api/SupplierOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunnas.lucas.supplier_orders_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierOrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
