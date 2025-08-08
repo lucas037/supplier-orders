ce97b055a05133ac8d6d39eac81af140f648a1db
Lucas <lugab037@gmail.com>
2025-08-08 14:02:20 -0300

    feat(controller): add ProductController with CRUD operations for products

--- src/main/java/com/dunnas/lucas/supplier_orders_api/controller/ProductController.java
+++ src/main/java/com/dunnas/lucas/supplier_orders_api/controller/ProductController.java
@@ -1,4 +1,4 @@
-package com.dunnas.lucas.supplier_orders_api.application.controller;
+package com.dunnas.lucas.supplier_orders_api.controller;
 
 import com.dunnas.lucas.supplier_orders_api.application.dto.ProductDTO;
 import com.dunnas.lucas.supplier_orders_api.application.service.ProductService;
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
