fe0a73a54284a68a8a974291360f922dee4a8147
Lucas <lugab037@gmail.com>
2025-08-08 13:04:35 -0300

    feat(Product): change patch to put on update

--- src/main/java/com/dunnas/lucas/supplier_orders_api/controller/ProductController.java
+++ src/main/java/com/dunnas/lucas/supplier_orders_api/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +47,7 @@ public class ProductController {
         return ResponseEntity.ok().body(HttpStatus.OK);
     }
 
-    @PatchMapping("/update/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<ProductDTO> update(@RequestBody ProductDTO product, @PathVariable Long id) {
         ProductDTO prodUpdated = prodServ.update(product, id);
         return ResponseEntity.ok().body(prodUpdated);
