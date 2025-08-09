4c342a5bf9fc6a3f04ebee89bdcda2bc639fa1fe
Lucas <lugab037@gmail.com>
2025-08-09 13:56:34 -0300

    feat(exception): add handler for BadCredentialsException to improve authentication error responses

--- src/main/java/com/dunnas/lucas/supplier_orders_api/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dunnas/lucas/supplier_orders_api/controller/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.dunnas.lucas.supplier_orders_api.controller.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,7 +15,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResp);
     }
 
-    
     @ExceptionHandler(JpaSystemException.class)
     public ResponseEntity<ErrorResponse> handleDataIntegrityViolation(JpaSystemException ex) {
         String fullMessage = ex.getRootCause() != null ? ex.getRootCause().getMessage() : ex.getMessage();
@@ -27,6 +27,12 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResp);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> badCredentialsException(BadCredentialsException ex) {
+        ErrorResponse errorResp = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "Usuário ou senha incorretos");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResp);
     }
 
 
